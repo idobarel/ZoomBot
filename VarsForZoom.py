4a8b8159d8d6a01e0e39c7de7349652ef11142c9
idobarel <55856610+idobarel@users.noreply.github.com>
2021-02-11 19:34:07 +0200

    Create VarsForZoom.py

---
+++ VarsForZoom.py
@@ -0,0 +1,35 @@
+subjects = ['math','hebrew','history','bible','arabic','programing','english','tomer']#Enter your own subjects
+
+days = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday']#Enter the days you have school
+
+links = ['https://edu-il.zoom.us/j/5776367490?pwd=elhvem8xVVU2b1hIMXg1ZUl1cnY5UT09'#Enter the links in the order of the
+,'https://edu-il.zoom.us/j/9785284720'                                             #subjects, if math is first then the 
+,'https://edu-il.zoom.us/j/4825786747'                                             #link of it will be first as well.
+,'https://edu-il.zoom.us/j/7254981076?pwd=cXdYYXErTWZMSjNZekw5bVE4SlVRZz09'
+,'https://edu-il.zoom.us/j/8521794024?pwd=bXZnL0doYlVuYmoxVHMvSE9TZVlCdz09'
+,'https://edu-il.zoom.us/j/3830225220?pwd=RjVUSlFTaTZKM3dGY3NuRWdPR0JpUT09',
+'https://edu-il.zoom.us/j/6186201384'
+,'https://zoom.us/j/4402301526?pwd=Rk8weE16MGEyZDhaZU43cnp5bExiZz09']
+
+
+#Enter the times & subjects that you lerning in the same order, at 09:00 im lerning programing at 10:10 math .... 
+Sunday_Times = ['09:00','10:10','13:40']#Dont forget to enter 0 before single digit number: 9 = 09 or the bot wont work.
+Sunday_Subjects=['programing','math','programing']
+#
+Monday_Times = ['08:15','10:10','13:40']
+Monday_Subjects=['english','bible','arabic']
+#
+Tuesday_Times =['08:15','10:10','12:45','14:25']
+Tuesday_Subjects=['math','hebrew','programing','programing']
+#
+Wednesday_Times =['08:15','12:00','14:25','16:00']
+Wednesday_Subjects=['arabic','history','english','tomer']
+#
+Thursday_Times =['08:15','11:00','12:45','14:25']
+Thursday_Subjects=['programing','history','hebrew','math']
+#
+Friday_Times =['08:15']
+Friday_Subjects=['math']
+
+#Fill For Every Day
+#And that is it!!
